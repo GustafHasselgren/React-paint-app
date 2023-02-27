955cb1f03391b1941e5a2410bf4184b51d284d6a
Gustaf Ericson <gustaf.ericson2@gmail.com>
2023-02-27 18:00:04 +0100

    structured fake-data, basic style for area

--- paint-app/src/components/area.js
+++ paint-app/src/components/area.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Area({name}) {
 
     return (
-        <div className='area'>{name}</div>
+        <div className='area'>
+            <p className="area-text">{name}</p>
+        </div>
     );
 
 }
--- paint-app/src/components/routes/schemeAreas.js
+++ paint-app/src/components/routes/schemeAreas.js
@@ -2,7 +2,22 @@ import { useState } from "react";
 import { useLocation } from "react-router-dom"
 import AreaList from "../areaList";
 
-var testAreas = ['Weapons', 'Armor', 'Clothes']
+var testAreas = [{area: 'Weapons', 
+                steps: 
+                    [{paint: 'red', method: 'base layer'}, 
+                    {paint: 'red', method: 'base layer'}, 
+                    {paint: 'red', method: 'base layer'}]},
+                {area: 'Armor', 
+                steps: 
+                    [{paint: 'red', method: 'base layer'}, 
+                    {paint: 'red', method: 'base layer'}, 
+                    {paint: 'red', method: 'base layer'}]},
+                {area: 'Clothes', 
+                steps: 
+                    [{paint: 'red', method: 'base layer'}, 
+                    {paint: 'red', method: 'base layer'}, 
+                    {paint: 'red', method: 'base layer'}]},
+                ];
 
 export default function SchemeAreas() {
     const location = useLocation();
