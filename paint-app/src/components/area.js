77b25a1996177b0de04a62c35672840667d027e2
Gustaf Ericson <gustaf.ericson2@gmail.com>
2023-02-28 16:40:11 +0100

    some styling and name changes

--- paint-app/src/components/area.js
+++ paint-app/src/components/area.js
@@ -3,13 +3,13 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function Area({data}) {
 
-    const [dropdown, setDropdown] = useState(false);
-    const toggleDropdown = () => {setDropdown(!dropdown)};
+    const [isDown, setIsDown] = useState(false);
+    const toggleDropdown = () => {setIsDown(!isDown)};
 
     return (
         <div className='area' onClick={toggleDropdown}>
             <p className="area-text">{data.area}</p>
-            {dropdown && <div  className='area-dropdown'>
+            {isDown && <div  className='area-dropdown'>
                 <div>
                     <span>Paint</span>
                     <span>Method</span>
